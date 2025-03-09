378dc312e66991b4b85eba9e86a58751a08639ad
ptthaothao <graciept2109@gmail.com>
2025-03-09 13:16:46 +0700

    update

--- src/views/client/contact/ContactView.vue
+++ src/views/client/contact/ContactView.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="w-full mt-12 overflow-hidden h-auto container">
-    <div class="w-full mt-5 mx-auto grid sm:place-content-center gap-y-6">
-      <h1 class="text-4xl font-bold  text-primary">CONTACT WITH US</h1>
+    <div class="w-full mt-5 mx-auto grid xl:place-content-center gap-y-6">
+      <h1 class="text-4xl font-bold text-primary">CONTACT WITH US</h1>
       <div class="space-y-3">
         <p>🏡 Địa chỉ: <span>Yên Nghĩa, Hà Đông, Hà Nội</span></p>
         <p>☎️ Liên hệ:: <span>0123456789</span></p>
         <p>💌 Emai: <span>admin@gmail.com</span></p>
         <p>🔍 Website: <span>admin@gmail.com</span></p>
       </div>
-      <div class="w-full h-[400px] sm:w-[800px]">
+      <div class="w-full h-[400px]">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14904.275296200025!2d105.7228624770364!3d20.949751090876642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3134526076102a63%3A0xac67de8995850ad5!2zWcOqbiBOZ2jEqWEsIEjDoCDEkMO0bmcsIEhhbm9pLCBWaWV0bmFt!5e0!3m2!1sen!2s!4v1741497569513!5m2!1sen!2s"
           class="w-full h-full"
