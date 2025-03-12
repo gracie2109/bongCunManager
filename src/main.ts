c4de419f1b354fb4619a1b7c290ab922f4a93014
ptthaothao <graciept2109@gmail.com>
2025-03-12 19:29:28 +0700

    rebiukd

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import i18n from "./i18n";
 import App from "./App.vue";
 import router from "./router";
 import { setupStore } from "@/stores";
+import "vue-swipe-actions/dist/vue-swipe-actions.css";
+
 const app = createApp(App);
 
 setupStore(app);
 app.use(router);
-app.use(i18n);
+app.use(i18n)
+
 app.mount("#app");
